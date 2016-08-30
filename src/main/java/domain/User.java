785481ae2517cb6f5b60c436c4d2eef26025e731
Evgeny Akulenko <Evgeny_Akulenko@epam.com>
2016-08-30 09:00:49 +0300

    implement helloWorld at the Spring boot

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,70 @@
+package domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private boolean sex;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/main/SpringWorkoutApp.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Evgeny_Akulenko on 8/26/2016.
+ */
+@SpringBootApplication
+public class SpringWorkoutApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorkoutApp.class, args);
+    }
+}
